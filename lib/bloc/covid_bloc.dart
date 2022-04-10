ab62876ea9cbcb449ee8f7598f7cf67429f78907
Angga ps <80311292+anggaps@users.noreply.github.com>
2022-04-10 23:34:41 +0700

    first commit

---
+++ lib/bloc/covid_bloc.dart
@@ -0,0 +1,28 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:ftips_bloc_fetch_api/resources/api_repository.dart';
+
+import '../models/covid_model.dart';
+
+part 'covid_event.dart';
+part 'covid_state.dart';
+
+class CovidBloc extends Bloc<CovidEvent, CovidState> {
+  CovidBloc() : super(CovidInitial()) {
+    final ApiRepository _apiRepository = ApiRepository();
+
+    on<CovidEvent>((event, emit) async {
+      // TODO: implement event handler
+      try {
+        emit(CovidLoading());
+        final mList = await _apiRepository.fetchCovidList();
+        emit(CovidLoaded(mList));
+        if (mList.error != null) {
+          emit(CovidError(mList.error));
+        }
+      } on NetworkError {
+        emit(const CovidError("Failed to fetch data. is your device online?"));
+      }
+    });
+  }
+}
---
+++ lib/pages/covid_page.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:ftips_bloc_fetch_api/bloc/covid_bloc.dart';
+import 'package:ftips_bloc_fetch_api/models/covid_model.dart';
+
+class CovidPage extends StatefulWidget {
+  const CovidPage({Key? key}) : super(key: key);
+
+  @override
+  State<CovidPage> createState() => _CovidPageState();
+}
+
+class _CovidPageState extends State<CovidPage> {
+  final CovidBloc _newsBloc = CovidBloc();
+
+  @override
+  void initState() {
+    _newsBloc.add(GetCovidList());
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('COVID-19 List')),
+      body: _buildListCovid(),
+    );
+  }
+
+  Widget _buildListCovid() {
+    return Container(
+      margin: const EdgeInsets.all(8.0),
+      child: BlocProvider(
+        create: (_) => _newsBloc,
+        child: BlocListener<CovidBloc, CovidState>(
+          listener: (context, state) {
+            if (state is CovidError) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text(state.message!),
+                ),
+              );
+            }
+          },
+          child: BlocBuilder<CovidBloc, CovidState>(builder: (context, state) {
+            if (state is CovidInitial) {
+              return _buildLoading();
+            } else if (state is CovidLoading) {
+              return _buildLoading();
+            } else if (state is CovidLoaded) {
+              return _buildCard(context, state.covidModel);
+            } else if (state is CovidError) {
+              return Container();
+            } else {
+              return Container();
+            }
+          }),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCard(BuildContext context, CovidModel model) {
+    return ListView.builder(
+      itemCount: model.countries!.length,
+      itemBuilder: (BuildContext context, int index) {
+        return Container(
+          margin: const EdgeInsets.all(8.0),
+          child: Card(
+            child: Container(
+              margin: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  Text("country : ${model.countries![index].country}"),
+                  Text(
+                      "Total Confirmed: ${model.countries![index].totalConfirmed}"),
+                  Text("Total Deaths: ${model.countries![index].totalDeaths}"),
+                  Text(
+                      "Total Recovered: ${model.countries![index].totalRecovered}"),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _buildLoading() => const Center(child: CircularProgressIndicator());
+}
